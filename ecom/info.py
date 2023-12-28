89ea91939619aaa488a941738ceb52d025f04a9a
Daniel Agufenwa <142681753+daniel-devi@users.noreply.github.com>
2023-12-28 12:41:35 +0000

    info.py

--- ecom/info.py
+++ ecom/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TILS = True
 EMAIL_HOST = 'stmp.gmail.com'
-EMAIL_HOST_USER = 'agufenwaobinna@gmail.com'
-EMAIL_HOST_PASSWORD = 'thgb bjmj hwvo limv'
+EMAIL_HOST_USER = '##########################'
+EMAIL_HOST_PASSWORD = '#####################'
 EMAIL_PORT = 587
